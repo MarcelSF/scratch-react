62c95e6c40169c898cf68d2f25aa16df651d9c04
marcel fonseca <marcelsf23br@gmail.com>
2019-03-25 10:14:13 -0300

    installed aws-amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "sa-east-1",
+    BUCKET: "marcel-learning-bucket"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://kykswubvsb.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_aPigwaQHV",
+    APP_CLIENT_ID: "34gkorak4v4qi0a2p5v08fr679",
+    IDENTITY_POOL_ID: "us-east-1:186d11b3-a16f-4815-a89b-e8241a27a2d0"
+  }
+};
